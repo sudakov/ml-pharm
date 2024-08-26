dca893ecdef637494d9e7dd613bf45316d8e4555
sudakov <vsudakov@bk.ru>
2024-08-26 09:03:08 +0300

    Update settings.py

--- Django/ml_pharm_web/settings.py
+++ Django/ml_pharm_web/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'ml_pharm_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',   # Используется PostgreSQL
-        'NAME': 'postgres', # Имя базы данных
-        'USER': 'postgres', # Имя пользователя
-        'PASSWORD': 'postgres', # Пароль пользователя
-        'HOST': 'pgdb', # Наименование контейнера для базы данных в Docker Compose
-        'PORT': '5432',  # Порт базы данных
+        'NAME': 'webpharm' # Имя базы данных
+
     }
 }
 
